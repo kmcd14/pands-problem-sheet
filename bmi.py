38ba0e2d44964494f79693d0bb349fe42b89d994
Katie Mc D <katieisanimdom@gmail.com>
2021-02-06 18:24:55 +0000

    added extra  comments to weekly task02

--- bmi.py
+++ bmi.py
@@ -1,5 +1,6 @@
 # bmi.py
-# calucates users bmi
+# weekly task02
+# this program calucates users bmi
 # Author: Katie Mc Donald
 
 # defining variables
@@ -8,6 +9,6 @@ height = float(input('{}, please enter your height in cm: '.format(name)))
 weight = float(input('Now enter your weight in kilograms: '))
 
 # BMI result
-bmi = weight / (height/100)**2
+bmi = weight / (height/100)**2  #changing cm into meters. dividing by 100 because 1m = 100cm
 
-print('{}, your BMI is {:.2f}'.format(name, bmi))
+print('{}, your BMI is {:.2f}'.format(name, bmi)) #.2f formats value returned to 2 decimal places
