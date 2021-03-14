5685378d86606da759e8235130b8967f50f228a2
Katie Mc D <katieisanimdom@gmail.com>
2021-03-14 13:42:13 +0000

    Added and edited comments. Added tabs to make make print statements easier to read

--- second_string.py
+++ second_string.py
@@ -1,14 +1,12 @@
 # second_string.py
-# weekly task03
-# a program that takes asks a user to input a string and outputs every second letter in reverse order.
-# author: Katie Mc Donald
+# This program takes an input of a string and outputs every second letter in reverse order.
+# Author: Katie Mc Donald
 
+# Getting the users input
 string = input('Please enter a sentance: ')
-print('you have entered: \n' + string)
+print('You have entered: \n\t\t' + string)
 
-# reversing the order of the string. -1 is always the last value/element
-# [starting value : end value : value you wish to move by]
-reversed_string = string[::-1]
-
-# printing the second letter of the reversed string
-print('your new string is:\n'  + reversed_string[::2])
+# Reversing the order of the string. -1 is always the last value/element
+reversed_string = string[::-1] # [starting value : end value : value to move by]
+# Printing the second letter of the reversed string
+print('Your new string is:\n\t\t'  + reversed_string[::2]) # [starting value : end value : value to move by]
