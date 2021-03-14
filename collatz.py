63a9107b0cb4a28f08a3ddcac3c9c742ccecb598
Katie Mc D <katieisanimdom@gmail.com>
2021-03-14 11:26:54 +0000

    Put code into a function. Cleaned up and added more comments

--- collatz.py
+++ collatz.py
@@ -1,23 +1,33 @@
 # collatz.py
-# This program asks the user to input any positive integer 
-# It will calculate next value by taking the current value
-# if it is even, divide it by two, but if odd, multiply it by three and add one.
+# This program takes the input of a positive integer  and calculates the
+# next value by taking the current value and if it is even \ 2. if odd * 3 + 1 
 # The program will end if the current value is one.
 # Author: Katie Mc Donald
 
 
-number = int(input('Please enter a posistive integer: '))
-new_number = 0
+# creating a function
+def collatz(number):
+    # main loop
+     while True:  # while the following conditions are true do the following
+            # finds if it's an even number (i.e. no remainder)                     
+            if (number % 2) == 0:  
+                new_number = number / 2
+                # prints new value of number.
+                print(int(new_number)) # Added int() as it was printing out a float otherwise   
+             # finds if it's an odd number (i.e. has a remainder)
+            elif (number % 2) == 1:   
+                new_number = (number * 3) + 1
+                # prints new value
+                print(int(new_number)) 
+            # reassigns the value of number. Without this the first result is printed on an infinite loop
+            number = new_number 
+            # breaks out of the loop when 1 is reached
+            if new_number == 1:          
+                break 
 
-while True:                              # while the following conditions are true do the following
-        if (number % 2) == 0:            # finds if it's an even number (no remainder)
-            new_number = number / 2
-            print(int(new_number))       
-        elif (number % 2) == 1:           # finds if it's an odd number
-            new_number = (number * 3) + 1
-            print(int(new_number)) 
-        
-        number = new_number
-        
-        if new_number == 1:                # breaks out of the loop when 1 is reached
-            break
+
+# defining a variable from user input and changing the string into an integer 
+user_number = int(input('Please enter a posistive integer: '))
+# calls the function
+collatz(user_number)
+print(collatz) 
