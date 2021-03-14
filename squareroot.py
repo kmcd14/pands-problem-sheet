4ad68c8fb73afcb19d797a5739ae00d610ba5e2f
Katie Mc D <katieisanimdom@gmail.com>
2021-03-14 14:25:24 +0000

    Cleaned up function. Made variable name choice easier to read. Edited and added more clarifying  comments

--- squareroot.py
+++ squareroot.py
@@ -1,22 +1,24 @@
 # squareroot.py
-# This program takes a positive floating-point number as input and outputs an approximation of its square root.
-# Newton Method:
-# Guess the approx root estimate of the number
-# Add the approx root to the (number divided approx root) divided by 2 
+# This program takes a positive floating-point number as input and outputs an approximation of its square root using the Newton Method
 # Author: Katie Mc Donald
 
-number_to_root= float(input('Please enter a posistive number: ' ))
-
-
-# Define the function that uses Newton's Method
+# Defining a function that uses Newton's Method
 def sqrt(number):
-    guess = number / 2  # getting an approx first root
-    while True:
-            better_guess = ((guess + (number / guess)) /2)  # gradually improving intial guess estimate
-            if better_guess == guess:
+    approx_root = number / 2  # Guessing the approx root estimate of the number
+    # While the following conditions are true do the following
+    while True: # Main loop
+        # Add the approx root to the (number divided approx root) divided by 2 
+            better_guess = ((approx_root + (number / approx_root)) /2)  # gradually improving intial guess estimate
+            # If the improved guess is equal to the approx guess beak out of the loop
+            if better_guess == approx_root:
                 # rounding to one decimal place to match example output given in task description 
-                print('Using Newtons Method we estimate the squareroot to be {:.1f}'.format( better_guess))
+                print('Using Newtons Method we estimate the squareroot of {} to be {:.1f}'.format(number_to_root, better_guess))
                 break 
-            guess = better_guess
+            # Reassigning approx root
+            approx_root = better_guess
     
+
+# Getting the users input as a float
+number_to_root= float(input('Please enter a posistive number: ' ))
+# Calling function   
 sqrt(number_to_root)
