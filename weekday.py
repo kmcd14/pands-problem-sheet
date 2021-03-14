c148c5f5be5f0952117e2752714202da4c0dfcfe
Katie Mc D <katieisanimdom@gmail.com>
2021-03-14 15:31:40 +0000

    Created a function for code. Edited and added more clarifying comments

--- weekday.py
+++ weekday.py
@@ -1,15 +1,19 @@
 # weekday.py
-# # Weekly Task 05
-# This program will outputs whether or not today is a weekday.
+# This program tells the user whether or not today is a weekday.
 
+ # Importing module that allows the manipulation of dates and times
 import datetime 
-# this function assigns an int to the days of the week 
+# This method assigns a number to the days of the week 
 # Mon = 0, Tues = 1, Wed = 2, Thurs = 3, Fri = 4, Sat = 5, Sun = 6
-day_of_week = datetime.datetime.today().weekday()
+day_of_week = datetime.datetime.today().weekday() # Assigning day to a variable 
 
-# if today is less than or equal to 4. i.e. Mon - Fri 
-if day_of_week <= 4:
-    print('Yes, today is a weekday :( ')
-# otherwise it's the weekend
-else:
-    print("WooHoo! It's the weekend! :)")
+# Defining a function
+def weekday(day):
+    # If today is less than or equal to 4 (i.e. Monday - Friday) it's a weekday.
+    if day <= 4: 
+        print('Yes, today is a weekday :( ')
+    else: # Otherwise it's the weekend (i.e. Saturday - Sunday)
+        print("WooHoo! It's the weekend! :)")
+
+# Calling the function
+weekday(day_of_week)
